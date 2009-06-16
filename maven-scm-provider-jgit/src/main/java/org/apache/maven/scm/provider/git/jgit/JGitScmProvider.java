7f512993f9b64229baeeaccd0ccefc051eeec26b
Mark Struberg <struberg@yahoo.de>
2009-06-16 21:47:55 +0200

    implement scm:changelog command for JGit

--- maven-scm-provider-jgit/src/main/java/org/apache/maven/scm/provider/git/jgit/JGitScmProvider.java
+++ maven-scm-provider-jgit/src/main/java/org/apache/maven/scm/provider/git/jgit/JGitScmProvider.java
@@ -26,6 +26,7 @@ import org.apache.maven.scm.provider.git.command.GitCommand;
 import org.apache.maven.scm.provider.git.command.info.GitInfoItem;
 import org.apache.maven.scm.provider.git.command.info.GitInfoScmResult;
 import org.apache.maven.scm.provider.git.jgit.command.add.JGitAddCommand;
+import org.apache.maven.scm.provider.git.jgit.command.changelog.JGitChangeLogCommand;
 import org.apache.maven.scm.provider.git.jgit.command.checkin.JGitCheckInCommand;
 import org.apache.maven.scm.provider.git.jgit.command.checkout.JGitCheckOutCommand;
 import org.apache.maven.scm.provider.git.jgit.command.status.JGitStatusCommand;
@@ -56,7 +57,7 @@ public class JGitScmProvider
     /** {@inheritDoc} */
     protected GitCommand getChangeLogCommand()
     {
-        return null;
+        return new JGitChangeLogCommand();
     }
 
     /** {@inheritDoc} */
